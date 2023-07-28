951c5ff1d75a5c4846ca371899fabdffe7352d21
levenfalk <nathalie.levenfalk@se.experis.com>
2023-07-28 16:13:17 +0200

    feat: change art and logout to navigation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { STORAGE_KEY_USER } from 'src/app/const/storage-keys';
+import { UpdateCardService } from 'src/app/services/update-card.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +11,31 @@ import { Router } from '@angular/router';
 export class NavbarComponent {
 
   public loginUrl :boolean;
+  isPixel:boolean;
 
-  constructor(private router:Router){
+  constructor(private router:Router, private readonly updateCardService:UpdateCardService){
     this.loginUrl = (this.router.url === '/') 
     console.log(this.loginUrl);
     router.events.subscribe((event) => 
       this.loginUrl = (this.router.url === '/')  
     )
+
+    this.isPixel = this.updateCardService.getIsPixel();
   }
 
-  
+  logoutClick(){
+    console.log("logout");
+    sessionStorage.removeItem(STORAGE_KEY_USER);
+    this.router.navigateByUrl("/");
+  }
+
+  changeArt(){
+    console.log("Change art");
+    this.isPixel = !this.updateCardService.getIsPixel();
+    console.log(this.isPixel  + " Change art " + this.updateCardService.getIsPixel());
+
+
+    this.updateCardService.emitEvent(this.isPixel);
+  }
 
 }
--- src/app/pages/trainer/trainer.page.ts
+++ src/app/pages/trainer/trainer.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { STORAGE_KEY_USER } from 'src/app/const/storage-keys';
 import { UpdateCardService } from 'src/app/services/update-card.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -10,24 +9,8 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./trainer.page.scss']
 })
 export class TrainerPage {
-  isPixel:boolean;
 
-  constructor(private readonly updateCardService:UpdateCardService, private readonly userService:UserService, private readonly router:Router  ){
-    this.isPixel = this.updateCardService.getIsPixel();
-  }
-
-  changeArt(){
-    console.log("Change art");
-    this.isPixel = !this.updateCardService.getIsPixel();
-    console.log(this.isPixel  + " Change art " + this.updateCardService.getIsPixel());
-
-
-    this.updateCardService.emitEvent(this.isPixel);
-  }
-  logoutClick(){
-    console.log("logout");
-    sessionStorage.removeItem(STORAGE_KEY_USER);
-    this.router.navigateByUrl("/");
+  constructor(private readonly userService:UserService, private readonly router:Router  ){
   }
 
 }
